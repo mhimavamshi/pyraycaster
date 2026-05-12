2442a2cb0a86ae965e486a3a0e49c480660e4573
mhimavamshi <himavamshi1212@gmail.com>
2026-05-13 00:21:23 +0530

    add cam translations

--- coordinate_mapper.py
+++ coordinate_mapper.py
@@ -9,3 +9,23 @@ def world_to_screen(origin, point):
 def world_to_screen_centre(surface, point):
     w, h = surface.get_size()
     return world_to_screen((w // 2, h // 2), point)
+
+
+def world_to_screen_cam(surface, camera_pos, world_pos):
+    w, h = surface.get_size()
+
+    rel_x = world_pos[0] - camera_pos[0]
+    rel_y = world_pos[1] - camera_pos[1]
+
+    return (
+        w // 2 + rel_x,
+        h // 2 - rel_y,
+    )
+
+def screen_to_world_cam(surface, camera_pos, screen_pos):
+    w, h = surface.get_size()
+
+    return (
+        camera_pos[0] + (screen_pos[0] - w // 2),
+        camera_pos[1] - (screen_pos[1] - h // 2),
+    )
